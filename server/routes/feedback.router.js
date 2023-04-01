b975f172c4268fe5228e9cdfe8e7fb76e9955a6b
James Jacobsen <edjcjacobsen@gmail.com>
2023-03-31 23:21:12 -0500

    added server side GET route and tested with postman

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 // GET route
+router.get('/', (req, res) => {
+    const queryText = `SELECT * FROM "feedback";`;
 
+    pool.query(queryText).then(result => {
+        console.log('recieved rows from db');
+        res.send(result.rows);
+    }).catch(err => {
+        console.log(err);
+    })
+})
 
 
 module.exports = router;
